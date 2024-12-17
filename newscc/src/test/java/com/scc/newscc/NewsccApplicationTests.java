ba576f3294612ee604ac0326a154012755ab3531
svjcan <svj.canada@gmail.com>
2024-12-17 14:52:45 -0500

    Add files via upload

---
+++ newscc/src/test/java/com/scc/newscc/NewsccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scc.newscc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
